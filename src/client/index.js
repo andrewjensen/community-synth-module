7c463179b6567d1b05b894840ef1173c485c7c45
Andrew Jensen <andrewjensen90@gmail.com>
2017-01-27 17:21:58 -0700

    Show note names, update on button press

--- src/client/index.js
+++ src/client/index.js
@@ -2,22 +2,59 @@
 
 const { Component, h, render } = preact;
 
+const NOTES_PER_OCTAVE = 12;
+const NOTE_NAMES = [
+  'C',
+  'C#',
+  'D',
+  'D#',
+  'E',
+  'F',
+  'F#',
+  'G',
+  'G#',
+  'A',
+  'A#',
+  'B'
+];
+
 const INITIAL_STEPS = [
+  36,
+  40,
+  43,
+  48,
+  60,
   64,
-  64,
-  64,
-  64,
-  64,
-  64,
-  64,
-  64,
+  67,
+  72,
 ];
 
 const CHANGE_INC = 'CHANGE_INC';
 const CHANGE_DEC = 'CHANGE_DEC';
 
 function noteName(midiValue) {
-  return 'C3';
+  const name = NOTE_NAMES[midiValue % NOTES_PER_OCTAVE];
+  const octave = Math.floor(midiValue / NOTES_PER_OCTAVE);
+  const result = name + octave;
+  return result;
+}
+
+function updateSteps(steps, index, action) {
+  return steps.map((step, i) => {
+    if (i !== index) return step;
+
+    // This is the step that changed!
+    if (action === CHANGE_INC) return increment(step);
+    else if (action === CHANGE_DEC) return decrement(step);
+  });
+}
+
+function increment(noteValue) {
+  return noteValue + 1;
+}
+
+function decrement(noteValue) {
+  return noteValue - 1;
 }
 
 class App extends Component {
@@ -31,8 +68,10 @@ class App extends Component {
     };
   }
 
-  _onChangeStep(index, mode) {
-    console.log('change step:', index, mode);
+  _onChangeStep(index, action) {
+    this.setState({
+      steps: updateSteps(this.state.steps, index, action)
+    });
   }
 
   render() {
