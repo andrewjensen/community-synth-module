c5a446c81d0db52cc499a041b5d65e433be4855d
Andrew Jensen <andrewjensen90@gmail.com>
2017-02-09 12:21:19 -0700

    Refactor state into a Store class

--- src/server/index.js
+++ src/server/index.js
@@ -2,60 +2,21 @@
 
 const express = require('express');
 const path = require('path');
+const Store = require('./store');
 
 const PUBLIC_DIR = path.resolve(__dirname, '../../public');
 const CLIENT_SRC_DIR = path.resolve(__dirname, '../client');
 
 module.exports = (port) => {
 
-  let _steps = [
-    36,
-    40,
-    43,
-    48,
-    60,
-    64,
-    67,
-    72,
-  ];
-  let _devices = 0;
-
-  function getState() {
-    return {
-      steps: _steps,
-      devices: _devices,
-    };
-  }
+  const store = new Store();
 
   Promise.resolve()
-    .then(() => startHttpServer(port))
-    .then(server => {
-
-      const io = require('socket.io')(server);
-
-      io.on('connection', (socket) => {
-        socket.on('client:initialize', () => {
-          _devices++;
-          socket.emit('server:initialize', getState());
-          io.emit('server:devices', _devices);
-        });
-
-        socket.on('disconnect', () => {
-          _devices--;
-          io.emit('server:devices', _devices);
-        });
-
-        socket.on('client:step:set', (data) => {
-          const step = data.step;
-          const value = data.value;
-          _steps[step] = value;
-          io.emit('server:step:set', { step, value });
-        });
-      });
-    });
+    .then(() => startServer(port))
+    .then(socketServer => handleSockets(socketServer, store));
 };
 
-function startHttpServer(port) {
+function startServer(port) {
   return new Promise((resolve, reject) => {
 
     const app = express();
@@ -73,8 +34,32 @@ function startHttpServer(port) {
         console.error('Error starting server:', err);
         reject(err);
       }
+
+      const socketServer = require('socket.io')(server);
       console.log(`Server listening on port ${port}`);
-      resolve(server);
+      resolve(socketServer);
+    });
+  });
+}
+
+function handleSockets(socketServer, store) {
+  socketServer.on('connection', (socket) => {
+    socket.on('client:initialize', () => {
+      store.incrementDevices();
+      socket.emit('server:initialize', store.getState());
+      socketServer.emit('server:devices', store.getDeviceCount());
+    });
+
+    socket.on('disconnect', () => {
+      store.decrementDevices();
+      socketServer.emit('server:devices', store.getDeviceCount());
+    });
+
+    socket.on('client:step:set', (data) => {
+      const step = data.step;
+      const value = data.value;
+      store.setStep(step, value);
+      socketServer.emit('server:step:set', { step, value });
     });
   });
 }
---
+++ src/server/store.js
@@ -0,0 +1,40 @@
+'use strict';
+
+class Store {
+  constructor() {
+    this.steps = [
+      36,
+      40,
+      43,
+      48,
+      60,
+      64,
+      67,
+      72,
+    ];
+    this.devices = 0;
+  }
+
+  // ACCESSORS -----------------------------------------------------------------
+
+  getState() {
+    return {
+      steps: this.steps,
+      devices: this.devices
+    };
+  }
+
+  getDeviceCount() { return this.devices; }
+
+  // ACTIONS -------------------------------------------------------------------
+
+  incrementDevices() { this.devices++; }
+
+  decrementDevices() { this.devices++; }
+
+  setStep(index, value) {
+    this.steps[index] = value;
+  }
+}
+
+module.exports = Store;
