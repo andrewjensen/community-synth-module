c74784d1d11c22b4830c4035a61f255715faf698
Andrew Jensen <andrewjensen90@gmail.com>
2017-02-09 09:01:58 -0700

    Refactor HTTP server initialization

--- src/server/index.js
+++ src/server/index.js
@@ -8,16 +8,6 @@ const CLIENT_SRC_DIR = path.resolve(__dirname, '../client');
 
 module.exports = (port) => {
 
-  const app = express();
-  const server = require('http').createServer(app);
-
-  app.use(express.static(PUBLIC_DIR));
-  app.use('/src', express.static(CLIENT_SRC_DIR));
-
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(PUBLIC_DIR, 'index.html'));
-  });
-
   let _steps = [
     36,
     40,
@@ -37,29 +27,54 @@ module.exports = (port) => {
     };
   }
 
-  server.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+  Promise.resolve()
+    .then(() => startHttpServer(port))
+    .then(server => {
 
-    const io = require('socket.io')(server);
+      const io = require('socket.io')(server);
 
-    io.on('connection', (socket) => {
-      socket.on('client:initialize', () => {
-        _devices++;
-        socket.emit('server:initialize', getState());
-        io.emit('server:devices', _devices);
-      });
+      io.on('connection', (socket) => {
+        socket.on('client:initialize', () => {
+          _devices++;
+          socket.emit('server:initialize', getState());
+          io.emit('server:devices', _devices);
+        });
 
-      socket.on('disconnect', () => {
-        _devices--;
-        io.emit('server:devices', _devices);
-      });
+        socket.on('disconnect', () => {
+          _devices--;
+          io.emit('server:devices', _devices);
+        });
 
-      socket.on('client:step:set', (data) => {
-        const step = data.step;
-        const value = data.value;
-        _steps[step] = value;
-        io.emit('server:step:set', { step, value });
+        socket.on('client:step:set', (data) => {
+          const step = data.step;
+          const value = data.value;
+          _steps[step] = value;
+          io.emit('server:step:set', { step, value });
+        });
       });
     });
-  });
 };
+
+function startHttpServer(port) {
+  return new Promise((resolve, reject) => {
+
+    const app = express();
+    const server = require('http').createServer(app);
+
+    app.use(express.static(PUBLIC_DIR));
+    app.use('/src', express.static(CLIENT_SRC_DIR));
+
+    app.get('/', (req, res) => {
+      res.sendFile(path.resolve(PUBLIC_DIR, 'index.html'));
+    });
+
+    server.listen(port, (err) => {
+      if (err) {
+        console.error('Error starting server:', err);
+        reject(err);
+      }
+      console.log(`Server listening on port ${port}`);
+      resolve(server);
+    });
+  });
+}
