c883774bb1e95385829865606933f25d1ea29894
Andrew Jensen <andrewjensen90@gmail.com>
2017-02-11 15:42:58 -0700

    Simulate state changes in synth

--- src/synth/community/src/community.ino
+++ src/synth/community/src/community.ino
@@ -1,26 +1,43 @@
 #define PIN D7
 
+// STATE -----------------------------------------------------------------------
+
+int mode = 0;
+int rgbState = 0;
+
 // LIFECYCLE -------------------------------------------------------------------
 
 void setup() {
   pinMode(PIN, OUTPUT);
 
-  Particle.subscribe("set_state", onMessage);
+  RGB.control(true);
 
-  lightOn();
-  delay(250);
-  lightOff();
-  delay(250);
-  lightOn();
-  delay(250);
-  lightOff();
-  delay(250);
-  lightOn();
-  delay(250);
-  lightOff();
+  Serial.begin(9600);
+
+  Serial.println("Hello world!");
+
+  Particle.subscribe("set_state", onMessage);
 }
 
 void loop() {
+  onClockSignal();
+  delay(1000);
+}
+
+// INPUT SIGNAL HANDLERS -------------------------------------------------------
+
+void onClockSignal() {
+  Serial.println("Clock signal received!");
+  rgbState = !rgbState;
+  if (rgbState) {
+    if (mode) {
+      colorBlue();
+    } else {
+      colorGreen();
+    }
+  } else {
+    colorOff();
+  }
 }
 
 // NETWORK EVENT HANDLERS ------------------------------------------------------
@@ -32,10 +49,11 @@ void onMessage(const char *event, const char *data) {
 }
 
 void handleSetState(const char *data) {
+  Serial.println("handling set_state");
   if (strcmp(data, "on") == 0) {
-    lightOn();
+    mode = 1;
   } else if (strcmp(data, "off") == 0) {
-    lightOff();
+    mode = 0;
   }
 }
 
@@ -47,4 +65,16 @@ void lightOn() {
 
 void lightOff() {
   digitalWrite(PIN, LOW);
+}
+
+void colorBlue() {
+  RGB.color(0, 0, 255);
+}
+
+void colorGreen() {
+  RGB.color(0, 255, 0);
+}
+
+void colorOff() {
+  RGB.color(0, 0, 0);
 }
